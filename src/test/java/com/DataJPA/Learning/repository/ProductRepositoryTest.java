7179449bd093755390400c4bd7ecbeefcb701a30
AshlinRosemary <ashlinrosemary@gmail.com>
2023-08-21 19:51:48 +0530

    Added & tested few CRUD operations

--- src/test/java/com/DataJPA/Learning/repository/ProductRepositoryTest.java
+++ src/test/java/com/DataJPA/Learning/repository/ProductRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -31,4 +32,48 @@ class ProductRepositoryTest {
         System.out.println(savedObject.getId());
         System.out.println(savedObject.toString());
     }
+    @Test
+    void updateMethod(){
+        //find entity by id
+        Long id=1L;
+        Product product=productRepository.findById(id).get();
+        //update entity
+        product.setName("UpdatedName_1");
+        product.setDescription("UpdatedDescription_1");
+        //save entity
+        productRepository.save(product);
+    }
+    @Test
+    void findbyIDMethod(){
+        Long id=1L;
+        Product product=productRepository.findById(id).get();
+    }
+    @Test
+    void saveAllMethod(){
+        Product product2=new Product();
+        product2.setName("Product2");
+        product2.setDescription("Product2 Description");
+        product2.setImageurl("product2 url");
+        product2.setSku("200AbC");
+        product2.setPrice(new BigDecimal(200));
+        product2.setActive(true);
+
+        Product product3=new Product();
+        product3.setName("Product3");
+        product3.setDescription("Product3 Description");
+        product3.setImageurl("product3 url");
+        product3.setSku("300AbC");
+        product3.setPrice(new BigDecimal(300));
+        product3.setActive(true);
+
+        productRepository.saveAll(List.of(product2,product3));
+    }
+    @Test
+    void findAllMethod() {
+        List<Product>  products = productRepository.findAll();
+        products.forEach((p) -> {
+            System.out.println(p.getName());
+        });
+    }
+
 }
